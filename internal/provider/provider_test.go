152e1fc26fdb2614504495e2fcdac82f418e7267
tae2089 <tae2089002@gmail.com>
2024-04-21 22:16:09 +0900

    :fire: Remove unused codes

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -3,19 +3,12 @@
 
 package provider
 
-import (
-	"testing"
-
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-)
-
 // testAccProtoV6ProviderFactories are used to instantiate a provider during
 // acceptance testing. The factory function will be invoked for every Terraform
 // CLI command executed to create a provider server to which the CLI can
 // reattach.
-var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"tae2089": providerserver.NewProtocol6WithError(New()()),
-}
+// var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
+// 	"tae2089": providerserver.NewProtocol6WithError(New()()),
+// }
 
-func testAccPreCheck(t *testing.T) {}
+// func testAccPreCheck(t *testing.T) {}
