218dde32f93cf3e94ec16e1e303fd9874b91d813
tae2089 <tae2089002@gmail.com>
2024-04-21 22:10:52 +0900

    :memo: Update docs

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -4,7 +4,7 @@ terraform {
       source = "hashicorp.com/tae2089/crypto"
     }
   }
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.8.0"
 }
 # Configure the connection details for the Inventory service
 provider "crypto" {}
